d9501a2586250187edfede4645587dd430cf04f6
Ravi Gupta <ravi1.gupta@intel.com>
2022-05-09 23:51:16 -0700

    add back nodes starter

--- main.go
+++ main.go
@@ -172,14 +172,14 @@ func autoScaling() {
 	for {
 		queueSize := fetchQueueSize()
 		queueSize = adjustQueueSizeDependingWhetherJobRequiringAllNodesIsRunning(queueSize)
-		log.Printf("%d jobs waiting to be executed\n", queueSize)
-		//if queueSize > 0 {
-		//	log.Printf("%d jobs waiting to be executed\n", queueSize)
-		//	enableMoreNodes(queueSize)
-		//} else if queueSize == 0 {
-		//	log.Println("No jobs in the queue")
-		//	disableUnnecessaryBuildBoxes()
-		//}
+		//log.Printf("%d jobs waiting to be executed\n", queueSize)
+		if queueSize > 0 {
+			log.Printf("%d jobs waiting to be executed\n", queueSize)
+			enableMoreNodes(queueSize)
+		} else if queueSize == 0 {
+			log.Println("No jobs in the queue")
+			disableUnnecessaryBuildBoxes()
+		}
 
 		log.Println("Iteration finished")
 		fmt.Println("")
